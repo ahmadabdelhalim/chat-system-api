2f37330e79e7acacb725f4f4818b8992316d906a
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2019-06-19 23:39:42 +0200

    add validations for application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,7 @@
 class Application < ApplicationRecord
   has_secure_token :access_token
+
+  # validations
+  validates :access_token, :name, presence: true
+  validates :access_token, uniqueness: true 
 end
