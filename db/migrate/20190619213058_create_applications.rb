ea5b075447ad366576a8e13f92d8c81f0809b7a2
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2019-06-19 23:36:09 +0200

    add application model and migration with name and access_token

---
+++ db/migrate/20190619213058_create_applications.rb
@@ -0,0 +1,11 @@
+class CreateApplications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :applications do |t|
+      t.string :access_token, null: false
+      t.string :name, null: false
+
+      t.timestamps
+    end
+    add_index :applications, :access_token, unique: true
+  end
+end
