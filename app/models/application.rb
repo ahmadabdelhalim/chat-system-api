5c574384369f7c79d599d4ac80125fc242ffe6fd
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2019-06-21 14:10:27 +0200

    remove presence validation for access token

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,6 +2,6 @@ class Application < ApplicationRecord
   has_secure_token :access_token
 
   # validations
-  validates :access_token, :name, presence: true
+  validates :name, presence: true
   validates :access_token, uniqueness: true 
 end
