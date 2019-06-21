807a569ed294f7cd9e7f03bee604265d25bc37fc
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2019-06-22 00:19:42 +0200

    add association between applications and chats

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,9 @@
 class Application < ApplicationRecord
   has_secure_token :access_token
 
+  # relations
+  has_many :chats, dependent: :destroy
+
   # validations
   validates :name, presence: true
   validates :access_token, uniqueness: true 
