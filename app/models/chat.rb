1224f68886f89c840ee3726578a036b36c192446
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2019-06-22 00:21:02 +0200

    create chat model with validations and association between application and chats. And auto increment the number

---
+++ app/models/chat.rb
@@ -0,0 +1,15 @@
+class Chat < ApplicationRecord
+  # relations
+  belongs_to :application
+
+  # validations
+  validates :application_id, :chat_number, presence: true
+  validates :chat_number, uniqueness: { scope: :application_id }
+
+  # callbacks
+  auto_increment :chat_number, 
+                              scope: [:application_id],
+                              initial: 1,
+                              force: true,
+                              lock: false
+end
