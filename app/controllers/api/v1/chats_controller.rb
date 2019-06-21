db3f8b68899693446a0146704ce80d34e73ff9f5
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2019-06-22 00:21:23 +0200

    implement chats controller

---
+++ app/controllers/api/v1/chats_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::ChatsController < ApplicationController
+  before_action :set_application, only: [:index, :show, :create, :update]
+  before_action :find_chat, only: [:show, :update]
+
+  def index
+    @chats = @application.chats
+    render json: @chats, each_serializer: Api::V1::ChatSerializer
+  end
+
+  def show
+    render json: @chat, status: :ok
+  end
+
+  def create
+    @chat = @application.chats.new
+    if @chat.valid?
+      ChatCreatorWorker.perform_async(@chat.application_id)
+      render json: { chat_number: {'Success'} }, status: :created
+    end
+  end
+
+  private
+
+  def set_application
+    @application = Application.find_by!(access_token: params[:application_access_token])
+  end
+
+  def find_chat
+    @chat = @application.chats.find_by!(number: params[:number])
+  end
+end
