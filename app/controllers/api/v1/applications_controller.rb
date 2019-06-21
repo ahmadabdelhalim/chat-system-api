9544b4ebca5b3dff86b432fc97849b0166a7f396
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2019-06-21 14:07:19 +0200

    implement CRUD functionality for applications controller

--- app/controllers/api/v1/applications_controller.rb
+++ app/controllers/api/v1/applications_controller.rb
@@ -1,2 +1,32 @@
-class Api::ApplicationsController < ApplicationController
+class Api::V1::ApplicationsController < ApplicationController
+  before_action :find_application, only: [:show, :update]
+
+  def index
+    @applications = Application.all
+    render json: @applications, each_serializer: Api::V1::ApplicationSerializer
+  end
+
+  def show
+    render json: @application
+  end
+
+  def create
+    @application = Application.create!(application_params)
+    render json: @application, status: :created
+  end
+
+  def update
+    @application.update(application_params)
+    render json: @application, status: :ok
+  end
+
+  private
+
+  def find_application
+    @application = Application.find_by!(access_token: params[:access_token]);
+  end
+
+  def application_params
+    params.require(:application).permit(:name)
+  end
 end
