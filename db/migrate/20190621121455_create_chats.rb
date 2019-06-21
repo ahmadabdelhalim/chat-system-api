c819837985918573ad194c2220695d97cdf9804a
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2019-06-22 00:13:31 +0200

    add chats migration

---
+++ db/migrate/20190621121455_create_chats.rb
@@ -0,0 +1,11 @@
+class CreateChats < ActiveRecord::Migration[5.2]
+  def change
+    create_table :chats do |t|
+      t.references :application, null:false, foreign_key: true
+      t.integer :chat_number, null: false, default: 0
+
+      t.timestamps
+    end
+    add_index :chats, [:application_id, :chat_number], unique: true
+  end
+end
