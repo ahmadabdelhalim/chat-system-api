e77122c999e40189978e8aeb3d58e53d4cc21534
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2019-06-22 00:24:13 +0200

    add sidekiq web route, and add routes for chats

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+  
   namespace :api do
     namespace :v1 do
-      resources :applications, param: :access_token, only: [:index, :show, :create, :update] 
+      resources :applications, param: :access_token, only: [:index, :show, :create, :update] do
+        resources :chats, param: :number, only: [:index, :show, :create, :update]
+      end
     end
   end
 end
