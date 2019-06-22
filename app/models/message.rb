3af7c8c9544504673bc60a612a9d644a6a723136
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2019-06-22 13:30:27 +0200

    add relations and validations and auto increment number for msesage model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,15 @@
 class Message < ApplicationRecord
+  # relations
   belongs_to :chat
+
+  # validations
+  validates :chat_id, :message_number, :body, presence: true
+  validates :message_number, uniqueness: { scope: :chat_id }
+
+  # callbacks
+  auto_increment :message_number, 
+                              scope: [:chat_id],
+                              initial: 1,
+                              force: true,
+                              lock: false
 end
