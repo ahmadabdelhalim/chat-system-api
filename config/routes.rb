158a12e07078b1fb6a3f657b350d8b70646035c4
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2019-06-22 13:33:11 +0200

    add resources for messages and change chat number param to chat_number param

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
-  
+
   namespace :api do
     namespace :v1 do
       resources :applications, param: :access_token, only: [:index, :show, :create, :update] do
-        resources :chats, param: :number, only: [:index, :show, :create, :update]
+        resources :chats, param: :chat_number, only: [:index, :show, :create] do
+          resources :messages, param: :message_number, only: [:index, :show, :create, :update]
+        end
       end
     end
   end
