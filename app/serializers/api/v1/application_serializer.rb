6802804f9da0cfbe1f221090d8c0a328f37a633b
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2019-06-22 17:37:13 +0200

    return chats count in application serializer

--- app/serializers/api/v1/application_serializer.rb
+++ app/serializers/api/v1/application_serializer.rb
@@ -1,4 +1,4 @@
 class Api::V1::ApplicationSerializer < ActiveModel::Serializer
-  attributes :access_token, :name, :created_at, :updated_at
+  attributes :access_token, :name, :chats_count, :created_at, :updated_at
   has_many :chats
 end
